d1514ef2a690558c28d97aa200eabdd1636f0528
Sajeel Aalam <imsajeel.dev@gmail.com>
2020-07-04 21:27:15 +0500

    Context API added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React from "react";
+
+import { Header } from "./components/Header";
+
+import { AddTransaction } from "./components/AddTransaction";
+import { Balance } from "./components/Balance";
+import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
+
+import { GlobalProvider } from "./context/GlobalState";
+
 import "./App.css";
-import Header from "./components/Header/Header";
-import Balance from "./components/Balance/Balance";
-import IncExp from "./components/IncExp/IncExp";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <div className="container">
-        <Balance />
-        <IncExp />
+    <GlobalProvider>
+      <div className="content">
+        <Header />
+        <div className="container">
+          <Balance />
+          <IncomeExpenses />
+          <TransactionList />
+          <AddTransaction />
+        </div>
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
